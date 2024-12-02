e04a5c8ee544596710b0b86d08c8d7bfcb7096a5
CelsoDelfino <50994973+CelsoDelfino@users.noreply.github.com>
2024-12-02 06:39:28 -0300

    criacao do servico do ViaCep que irá fazer as requisicoes do CEP

---
+++ Fullstack-backend/src/main/java/api/java/Fullstack_backend/utils/ConfigUtils.java
@@ -0,0 +1,9 @@
+package api.java.Fullstack_backend.utils;
+
+public class ConfigUtils {
+
+    public static final String URL_VIA_CEP = "https://viacep.com.br/ws/%s/json/";
+
+    public static final String ERRO = "erro";
+    public static final String CEP_NAO_LOCALIZADO = "CEP nao localizado";
+}
